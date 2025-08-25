0fae506f9332a3bf63cdf347d4b04e960bd6bedb
Adit0507 <adi05072002@gmail.com>
2025-08-25 20:23:05 +0530

    working on broker

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,9 +1,13 @@
 package broker
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"sync"
 
 	"github.com/Adit0507/message-queue-implementation/pkg/config"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
 
@@ -20,4 +24,48 @@ type Broker struct {
 type MessageDispatch struct {
 	Queue   string
 	Message *Message
+}
+
+func NewBroker(cfg *config.BrokerConfig) *Broker {
+	return &Broker{
+		config:            cfg,
+		queues:            make(map[string]Queue),
+		connections:       make(map[string]*websocket.Conn),
+		pendingMessages:   make(map[string]*Message),
+		messageDispatcher: make(chan *MessageDispatch, 1000),
+		upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true //allowing connections from any origin
+			},
+		},
+	}
+}
+
+func (b *Broker) Start() error {
+	go b.startMessageDispatcher()
+
+	go b.startCleanupRoutine()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", b.handleWebsocket)
+	router.HandleFunc("/queues", b.HandleGetQueues).Methods("GET")
+	router.HandleFunc("/queues/{name}/stats", b.handleGetQueueStats).Methods("GET")
+	router.HandleFunc("/health", b.handleHealth).Methods("GET")
+
+	addr := fmt.Sprintf("%s:%d", b.config.Host, b.config.Port)
+	log.Printf("Message Queue Broker starting on %s", addr)
+
+	return http.ListenAndServe(addr, router)
+}
+
+
+func (b *Broker) handleWebsocket(w http.ResponseWriter, r* http.Request) {
+	conn, err := b.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("websocket upgrade failed: %v", err)
+		return
+	}
+	
+
+
 }
